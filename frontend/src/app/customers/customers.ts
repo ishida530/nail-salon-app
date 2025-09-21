d6615ac75001a652fca04706563f1dd8a9801823
ishida530 <ishida530@gmail.com>
2025-09-21 13:47:33 +0200

    fix mistakes with display localhost without ngnix

---
+++ frontend/src/app/customers/customers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers',
+  standalone: true,
+  templateUrl: './customers.html',
+  styleUrls: ['./customers.scss']
+})
+export class Customers {}
